e08689627e92c9f25a74a8e51fbb29536124b1ac
rayhanp1402 <rayhan.hanif14maulana@gmail.com>
2025-02-24 20:08:05 +0700

    style: Loaded/unloaded ROM indicator

--- scripts/src/emulator.ts
+++ scripts/src/emulator.ts
@@ -1,14 +1,26 @@
 import { CHIP8 } from "./chip8.js";
 import { Disassembler } from "./disassembler.js";
 
+const romIndicatorLight = document.getElementById("rom-indicator") as HTMLElement;
+const romStatusText = document.getElementById("rom-status-text") as HTMLElement;
+
 fetch("signed_url")
-    .then(response => response.arrayBuffer())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Failed to fetch ROM: ${response.status} ${response.statusText}`);
+        }
+        return response.arrayBuffer();
+    })
     .then(buffer => {
         const chip8 = new CHIP8();
         chip8.loadROM(new Uint8Array(buffer));
-        console.log("ROM loaded successfully!");
+        
+        romIndicatorLight.style.backgroundColor = '#00FF33';
+        romStatusText.innerText = `Loaded 'IBM Logo.ch8' ROM`;
 
         const disassembler = new Disassembler(chip8);
         chip8.run();
     })  
-    .catch(error => console.error("Error loading ROM:", error));
+    .catch(error => {
+        console.error("Error loading ROM:", error)
+    });
