afd311f86ea7a5d5ff08447a77d03d86530a4808
rayhanp1402 <rayhan.hanif14maulana@gmail.com>
2025-03-01 19:31:33 +0700

    init: backend

---
+++ chip8_rom_server/src/main/java/com/rayhanp1402/chip8_rom_server/Chip8RomServerApplication.java
@@ -0,0 +1,13 @@
+package com.rayhanp1402.chip8_rom_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chip8RomServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chip8RomServerApplication.class, args);
+	}
+
+}
---
+++ chip8_rom_server/src/test/java/com/rayhanp1402/chip8_rom_server/Chip8RomServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayhanp1402.chip8_rom_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chip8RomServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
